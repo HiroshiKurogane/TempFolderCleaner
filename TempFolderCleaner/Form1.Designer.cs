137b1cda0af91e0e0d1ae0ec23e51b2f2e947bb1
KilledKingFireWolf <aczelmihaly1@gmail.com>
2024-12-07 10:44:47 +0100

    Add project files.

---
+++ TempFolderCleaner/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace TempFolderCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            checkBox5 = new CheckBox();
+            checkBox6 = new CheckBox();
+            checkBox7 = new CheckBox();
+            button2 = new Button();
+            button3 = new Button();
+            checkBox8 = new CheckBox();
+            label1 = new Label();
+            button4 = new Button();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            checkBox9 = new CheckBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button1.Font = new Font("Segoe UI", 12F);
+            button1.Location = new Point(265, 172);
+            button1.Margin = new Padding(0);
+            button1.Name = "button1";
+            button1.Size = new Size(250, 89);
+            button1.TabIndex = 0;
+            button1.Text = "Clean Temp Folders";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(71, 17);
+            checkBox1.Margin = new Padding(3, 2, 3, 2);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(63, 19);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "Roblox";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(144, 17);
+            checkBox2.Margin = new Padding(3, 2, 3, 2);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(59, 19);
+            checkBox2.TabIndex = 2;
+            checkBox2.Text = "Steam";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(214, 17);
+            checkBox3.Margin = new Padding(3, 2, 3, 2);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(84, 19);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "EpicGames";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(310, 17);
+            checkBox4.Margin = new Padding(3, 2, 3, 2);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(76, 19);
+            checkBox4.TabIndex = 2;
+            checkBox4.Text = "Battle.net";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(38, 348);
+            checkBox5.Margin = new Padding(3, 2, 3, 2);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(255, 19);
+            checkBox5.TabIndex = 2;
+            checkBox5.Text = "Delete duplicated files in the following path";
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new Point(397, 17);
+            checkBox6.Margin = new Padding(3, 2, 3, 2);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(186, 19);
+            checkBox6.TabIndex = 2;
+            checkBox6.Text = "Windows Error Reporting Logs";
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            checkBox7.AutoSize = true;
+            checkBox7.Location = new Point(589, 17);
+            checkBox7.Margin = new Padding(3, 2, 3, 2);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new Size(125, 19);
+            checkBox7.TabIndex = 2;
+            checkBox7.Text = "Visual Studio Code";
+            checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button2.Font = new Font("Segoe UI", 12F);
+            button2.Location = new Point(688, 130);
+            button2.Margin = new Padding(0);
+            button2.Name = "button2";
+            button2.Size = new Size(104, 31);
+            button2.TabIndex = 0;
+            button2.Text = "Select all";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button3.Font = new Font("Segoe UI", 12F);
+            button3.Location = new Point(8, 130);
+            button3.Margin = new Padding(0);
+            button3.Name = "button3";
+            button3.Size = new Size(104, 31);
+            button3.TabIndex = 0;
+            button3.Text = "Deselect all";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // checkBox8
+            // 
+            checkBox8.AutoSize = true;
+            checkBox8.Location = new Point(71, 40);
+            checkBox8.Margin = new Padding(3, 2, 3, 2);
+            checkBox8.Name = "checkBox8";
+            checkBox8.Size = new Size(66, 19);
+            checkBox8.TabIndex = 2;
+            checkBox8.Text = "Discord";
+            checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(125, 325);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Path";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(38, 322);
+            button4.Margin = new Padding(3, 2, 3, 2);
+            button4.Name = "button4";
+            button4.Size = new Size(82, 22);
+            button4.TabIndex = 4;
+            button4.Text = "Select";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // checkBox9
+            // 
+            checkBox9.AutoSize = true;
+            checkBox9.Location = new Point(143, 40);
+            checkBox9.Margin = new Padding(3, 2, 3, 2);
+            checkBox9.Name = "checkBox9";
+            checkBox9.Size = new Size(109, 19);
+            checkBox9.TabIndex = 2;
+            checkBox9.Text = "Browsers Cache";
+            checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(label1);
+            Controls.Add(checkBox9);
+            Controls.Add(checkBox8);
+            Controls.Add(checkBox7);
+            Controls.Add(checkBox6);
+            Controls.Add(checkBox5);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Temp Folder Cleaner";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+        private CheckBox checkBox7;
+        private Button button2;
+        private Button button3;
+        private CheckBox checkBox8;
+        private Label label1;
+        private Button button4;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private CheckBox checkBox9;
+    }
+}
